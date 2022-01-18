df1753f174a0e8a5f49a5fba3437c9f38b02d4b0
Nova <eldanadesalgne@gmail.com>
2022-01-18 12:16:06 -0500

    Pushing OG folder and file set up

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const fs = require('fs');
+const generateTeam = require('./src/generateTeam');
